eb5217bb92e9040a011b36a60f4cd38881e26626
Md. Shahriar Kabir Himel <death44635@gmail.com>
2024-09-09 18:00:27 +0600

    minor

--- Inventory.DTO/DTOs/StockDto.cs
+++ Inventory.DTO/DTOs/StockDto.cs
@@ -9,20 +9,23 @@ namespace Inventory.DTO.DTOs
 {
     public class StockDto
     {
+        [Key]
         public Guid skuId { get; set; }
 
+        [Display(Name = "Stock Status")]
         public string StockStatus { get; set; } = "";
 
+        [Display(Name = "Received Time")]
+        [Required]
         public DateTime StockReceiveDate { get; set; }
-
         public long StockTotalCost { get; set; } = 0;
 
+        [Display(Name = "Supplier Name")]
         public int? SupplierId { get; set; }
 
+        [Display(Name = "Number Of Products")]
+        [Required]
         public int ProductNumber { get; set; }
-
-
-
 
     }
 }
--- Inventory.DTO/ViewModels/StockViewModel.cs
+++ Inventory.DTO/ViewModels/StockViewModel.cs
@@ -1,12 +1,11 @@
 ﻿using Inventory.DTO.DTOs;
-using Inventory.DTO.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Inventory.DTO.ViewModels
 {
     public class StockViewModel
     {
-        public Stock stock { get; set; }
+        public StockDto stock { get; set; }
         public List<StockProductDto> products { get; set; }
         public SelectList Categories { get; set; }
     }
